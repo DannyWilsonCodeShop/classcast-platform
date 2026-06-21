a54b0397b406ca37951f4789bf73c530e36373a1
Danny Wilson <dannywilson@example.com>
2026-06-20 20:27:15 -0400

    Update bundle ID to com.myclasscast.ios for App Store registration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.classcast.app',
+  appId: 'com.myclasscast.ios',
   appName: 'ClassCast',
   webDir: 'out',
   server: {
